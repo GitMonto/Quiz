bd2f962d3bce0b96241365f1d76715461cb2cc98
Marko Milovic <mmilovic21@gmail.com>
2022-08-08 14:03:24 +0100

    Quiz Finish

--- app/index.js
+++ app/index.js
@@ -1,1 +1,35 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+const customer = {
+  firstname: "Marko",
+  lastname: "Milovic",
+  phonenumber: "07876132152",
+  email: "mmilovic21@gmail.com",
+};
+
+customer.address = {
+  street: "Blenheim Crescent",
+  city: "London",
+  state: "N/A",
+  zipcode: "W11 1NZ",
+};
+
+console.log(`Welcome, ${customer.firstname} ${customer.lastname}!`);
+
+const product = {
+  name: "product",
+  price: 100,
+  description: "I am the product",
+  quantity: 10,
+};
+customer.quantity = 7;
+customer.total = customer.quantity * product.price;
+console.log(customer);
+console.log(`Total: ${customer.total}`);
+
+if (customer.total > 100) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+product.quantity -= 7;
+console.log(product.quantity);
